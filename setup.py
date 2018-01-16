9cedaaef59a07ffbfe464ce733ccfd402de93526
Rachel Alpert <ambitious.fishus@gmail.com>
2018-01-16 14:31:19 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
    name='diceroller',
-   version='0.1.0',
+   version='0.2.0',
    description='A simple DnD style dice roller.',
    author='Rachel Alpert',
    author_email='universepup@google.com',
    url='https://github.com/rlalpert/diceroller',
    download_url='https://github.com/rlalpert/diceroller/archive/0.1.0.tar.gz',
-   keywords=['dice', 'roll', 'dnd', 'd&d'],
+   keywords=['dice', 'roll', 'dnd', 'd&d', 'tabletop'],
    packages=['diceroller'],  
    install_requires=[], 
    scripts=[]
